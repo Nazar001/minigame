2234df24a03d115dd855898083591b0e19aa0bcf
Mykola Shwayko <kolik2001@gmail.com>
2019-09-08 14:43:31 +0300

    Plate jumps

--- src/components/Games/PongPage.jsx
+++ src/components/Games/PongPage.jsx
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { withRouter } from "react-router";
 import './PongPage.scss';
 import { Stage, Layer, Rect, Circle, Group, Arrow, Tag, Text, Label } from 'react-konva';
-import Konva from 'react-konva';
 
 
 
@@ -10,63 +9,72 @@ class PongPage extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            ballDirection: false
+            ballDirection: false,
+            ballX: document.documentElement.clientWidth / 2 - 5,
+            ballY: document.documentElement.clientHeight / 2 - 5,
+            isPlaying: false,
+            leftScore: 0,
+            rightScore: 0
         }
     }
 
     moveBall = () => {
-        let fly = Math.random() * 1000;
-        if (fly / 1000 > 0.5) this.bottom(this.Circle);
-        if (fly / 1000 <= 0.5) this.top(this.Circle);
-    }
-
-    bottom = (a) => {
-        let endY = document.documentElement.clientHeight - 20;
-        let fly = Math.random() * 1000;
-        if (this.state.ballDirection === true) fly *= -1;
-        a.to({
-            x: this.Circle.attrs.x - fly,
-            y: endY,
-            duration: 1,
-            onFinish: () => {
-                if (a.attrs.y === endY) {
-                    this.top(a);
-                }
-            }
+        this.setState({
+            isPlaying: true
         })
-        if (a.attrs.x <= 0) {
-            a.to({
-                x: document.documentElement.clientWidth / 2 - 5,
-                y: document.documentElement.clientHeight / 2 - 5,
-                duration: 0
+        if (this.state.isPlaying === true) {
+            let fieldH = document.documentElement.clientHeight;
+            console.log(this.Rectangle)
+            let scaleX = Math.random() * 3;
+            let scaleY = Math.random() * 3;
+            let temp = setInterval(() => {
+                this.setState({
+                    ballX: this.state.ballX += scaleX,
+                    ballY: this.state.ballY += scaleY
+                })
+                // Мячик по-вертикали
+                if (this.state.ballY < 0 || this.state.ballY + 20 > fieldH) {
+                    scaleY = -scaleY;
+                }
+                // Мячик направо
+                if (this.state.ballX > document.documentElement.clientWidth - 16) {
+                    scaleX = Math.random() * -3;
+                    scaleY = Math.random() * 3;
+                    let temp = this.state.leftScore + 1;
+                    console.log(temp)
+                    this.setState({
+                        ballX: document.documentElement.clientWidth / 2 - 5,
+                        ballY: document.documentElement.clientHeight / 2 - 5,
+                        leftScore: temp
+                    })
+                }
+                // Мячик налево
+                if (this.state.ballX < 16) {
+                    scaleX = Math.random() * 3;
+                    scaleY = Math.random() * 3;
+                    let temp = this.state.rightScore + 1;
+                    this.setState({
+                        ballX: document.documentElement.clientWidth / 2 - 5,
+                        ballY: document.documentElement.clientHeight / 2 - 5,
+                        rightScore: temp
+                    })
+                }
+                if (this.touch(this.Circle, this.Rect) === true || this.touch(this.Circle, this.Rectangle) === true) {
+                    scaleX = -scaleX * 1.1;
+                }
             })
         }
-
     }
 
-    top = (a) => {
-        let fly = Math.random() * 1000;
-        if (this.state.ballDirection === true) fly *= -1;
-        a.to({
-            x: this.Circle.attrs.x - fly,
-            y: 20,
-            duration: 1,
-            onFinish: () => {
-                if (a.attrs.y === 20) {
-                    this.bottom(a);
-                }
-            }
-        })
-        if (a.attrs.x <= 0) {
-            a.to({
-                x: document.documentElement.clientWidth / 2 - 5,
-                y: document.documentElement.clientHeight / 2 - 5,
-                duration: 0
-            })
-        }
-
+    touch = (a, b) => {
+        if (a.attrs.x + a.attrs.radius > b.attrs.x &&
+            a.attrs.x < b.attrs.x + b.attrs.width &&
+            a.attrs.y + a.attrs.radius > b.attrs.y &&
+            a.attrs.y < b.attrs.y + b.attrs.height)
+            return true;
+        else
+            return false;
     }
-
     render() {
         return (
             <Stage width={document.documentElement.clientWidth} height={document.documentElement.clientHeight}>
@@ -92,13 +100,14 @@ class PongPage extends Component {
                         ref={node => {
                             this.Circle = node;
                         }}
-                        x={document.documentElement.clientWidth / 2 - 5}
-                        y={document.documentElement.clientHeight / 2 - 5}
-                        radius={20}
+                        x={this.state.ballX}
+                        y={this.state.ballY}
+                        radius={16}
                         fill="white"
                         onClick={() => this.moveBall(this.props.node)}
                     />
                     <Rect
+                        id='leftPlayer'
                         ref={node => {
                             this.Rect = node;
                         }}
@@ -108,6 +117,43 @@ class PongPage extends Component {
                         height={90}
                         fill='black'
                     />
+
+                    <Rect
+                        id='rightPlayer'
+                        ref={node => {
+                            this.Rectangle = node;
+                        }}
+                        x={document.documentElement.clientWidth - 35}
+                        y={document.documentElement.clientHeight / 2 - 45}
+                        width={15}
+                        height={90}
+                        fill='black'
+                    />
+                </Layer>
+                <Layer>
+                    <Label>
+                        <Text
+                            x={document.documentElement.clientWidth / 2 - 120}
+                            fontSize={90}
+                            text={`${this.state.leftScore}`}
+                            wrap="char"
+                            align="center"
+                        />
+                        <Text
+                            x={document.documentElement.clientWidth / 2 - 43}
+                            fontSize={90}
+                            text=' : '
+                            wrap="char"
+                            align="center"
+                        />
+                        <Text
+                            x={document.documentElement.clientWidth / 2 + 30}
+                            fontSize={90}
+                            text={`${this.state.rightScore}`}
+                            wrap="char"
+                            align="center"
+                        />
+                    </Label>
                 </Layer>
             </Stage>
         );
