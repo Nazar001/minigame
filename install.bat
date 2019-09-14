1df13e162b3748ad305f025b38cc6a6344837824
Nazar001 <rezik013@ukr.net>
2019-09-14 17:55:01 +0300

    Update commit 1: Add install.bat 2: Add start.bat 3: Add Backend

---
+++ install.bat
@@ -0,0 +1,7 @@
+cd backend
+cls
+start cmd.exe /k "npm i&exit"
+cd ..
+cd frontend
+cls
+start cmd.exe /k "npm i&exit"
---
+++ start.bat
@@ -0,0 +1,7 @@
+cd backend
+cls
+start cmd.exe /k "node server.js"
+cd ..
+cd frontend
+cls
+start cmd.exe /k "npm start"
